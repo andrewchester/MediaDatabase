4e6e2a870fe908463cf0f42d87c9e04f37d333ba
U-BSD\476724 <476724@SHS-10L74C39E.beaverton.k12.or.us>
2018-09-21 09:10:33 -0700

    Started addItem()

--- main.cpp
+++ main.cpp
@@ -1,13 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <string.h>
 
 #include "media.h"
 #include "movie.h"
 #include "videogame.h"
 #include "music.h"
 
+void makeLower(char* str)
+{
+  int asciiValue = 0;
+  for(int i = 0; i < strlen(str); i++)
+  {
+    asciiValue = (int)str[i];
+    if(asciiValue >= 65 && asciiValue <= 90)
+      str[i] = (char)(asciiValue + 32);
+  }
+}
+
+void addItem(std::vector<Media*>* media)
+{
+  char type[9];
+  std::cout << "Enter the type";
+}
+
 int main()
 {
-  std::vector<Media> media = new std::vector<Media>();
+  std::vector<Media*> media = new std::vector<Media*>();
+  char command[10];
+
+  std::cout << "Enter a command(PRINT, ADD, DELETE, SEARCH): ";
+  std::cin.get(command, 10);
+  std::cin.clear();
+  std::cin.ignore(100, '\n');
+  makeLower(&command);
+
+  if(strcmp(command, "add") == 1)
+  {
+    addItem(&media);
+  }
   
   return 0;
 }
