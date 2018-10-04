af4b9f043f29e6411850248baf3bb84c38218a33
Andrew Chester <39309332+andrewchester@users.noreply.github.com>
2018-10-03 19:25:35 -0700

    Added comments

--- movie.cpp
+++ movie.cpp
@@ -1,9 +1,11 @@
+//Implementation of movie.h
 #include "movie.h"
 
 int Movie::getType()
 {
-  return 0;
+  return 0; //Return 0 since it's type movie
 }
+//Setter for author
 void Movie::setAuthor(char* author, int size)
 {
   for(int i = 0; i < size; i++)
