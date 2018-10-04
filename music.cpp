fdb71e47507767e7141437b60c2dba68155bb453
Andrew Chester <39309332+andrewchester@users.noreply.github.com>
2018-10-03 19:27:14 -0700

    Added comments

--- music.cpp
+++ music.cpp
@@ -1,9 +1,11 @@
+//Implementation of music.h
 #include "music.h"
 
 int Music::getType()
 {
-  return 1;
+  return 1; //Return 1 since it's type music
 }
+//Setter for author
 void Music::setAuthor(char* author, int size)
 {
   for(int i = 0; i < size; i++)
