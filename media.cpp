935de72557ddf5fd3c4e159debce61b2ed745e4d
Andrew Chester <39309332+andrewchester@users.noreply.github.com>
2018-10-03 19:23:56 -0700

    Added comments

--- media.cpp
+++ media.cpp
@@ -1,15 +1,9 @@
+//Implementing media.h
 #include "media.h"
 
+//Change the title of a item
 void Media::setTitle(char* string, int size)
 {
   for(int i = 0; i < size; i++)
     this->title[i] = string[i];
-}
-Media::Media()
-{
-
-}
-Media::~Media()
-{
-  
 }
