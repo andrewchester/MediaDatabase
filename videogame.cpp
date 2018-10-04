4479491dbb7736de1bbefde92cf0786722b8e520
Andrew Chester <39309332+andrewchester@users.noreply.github.com>
2018-10-03 19:28:20 -0700

    Added comments

--- videogame.cpp
+++ videogame.cpp
@@ -1,9 +1,11 @@
+//Implementation of videogame.h
 #include "videogame.h"
 
 int VideoGame::getType()
 {
-  return 2;
+  return 2;//Return 2 since it's type videogame
 }
+//Setter for author(really publisher since its a videogame)
 void VideoGame::setAuthor(char* author, int size)
 {
   for(int i = 0; i < size; i++)
